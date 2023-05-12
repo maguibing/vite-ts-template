58a8ce096de07a6461d7e61b8b4d4d7dbf6da7ab
maguibing <kuse012@163.com>
2023-05-12 14:27:06 +0800

    update readme.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Vue 3 + Ts + Vite
 
 node: version : 16.20.0  prettier open
+文档地址: https://juejin.cn/post/7228978346502946874#heading-0
--- src/store/user.ts
+++ src/store/user.ts
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 
 // defineStore 第一个参数是id，必需且值唯一
 export const useHomeStore = defineStore('home', () => {
-  const msg = ref('这是首页的pinia');
+  const msg = ref<string>('这是首页的pinia');
   return {
     msg,
   };
